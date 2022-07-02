7ac0a94581722c5902fb0c7f887021fc1b16c752
Alicia <MAliciaHadisaputra@outlook.com>
2022-07-02 22:20:02 +1000

    created App bar

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import {Grid, Divider, Container} from "@mui/material";
 import React from "react";
+import ResponsiveAppBar from "../AppBar/ResponsiveAppBar";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
@@ -12,7 +13,8 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <Container>
     <Grid container direction="column">
       <Grid  item>
-        <Header />
+        {/* <Header /> */}
+        <ResponsiveAppBar/>
       </Grid>
       <Grid item>
           <Divider variant="middle" sx={{backgroundColor:"primary.light"}}/>
