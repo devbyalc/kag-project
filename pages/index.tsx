52f6be70ce6cab476006aed945e4236a6032e302
Alicia <MAliciaHadisaputra@outlook.com>
2022-07-07 00:01:46 +1000

    updated layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
-import type { NextPage } from 'next'
-import Banner from '../components/Banner/Banner'
+import type { NextPage } from "next";
+import Banner from "../components/Banner/Banner";
+import Card from "../components/Card/Card";
+import CardList from "../components/Card/CardList";
 
 const Home: NextPage = () => {
   return (
-   <Banner/>
-  )
-}
+    <div className="grid gap-5">
+      <div>
+      <Banner />
+<div className="divider"></div>
+      </div>
+      <div>
+      <CardList />
+      <div className="divider"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
