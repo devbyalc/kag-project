2a53f73d53e8adc62bd49e45f95cbb14126821cb
Alicia <MAliciaHadisaputra@outlook.com>
2022-07-09 02:29:33 +1000

    fix searchbar icon

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -29,10 +29,10 @@ const SearchBar: React.FC = () => {
     </div>
     {/* show search icon on mobile */}
     <div className="dropdown dropdown-end">
-    <button className="md:hidden btn btn-ghost btn-circle text-main-900">
+    <button tabIndex={0} className="md:hidden btn btn-ghost btn-circle text-main-900">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
     </button>
-    <div className="dropdown-content menu md:block form-control">
+    <div tabIndex={0} className="dropdown-content menu md:block form-control">
       <div className="input-group">
         <input
           type="text"
